a4736f147d2350f214364a5175b582360d0f06e4
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-16 10:11:15 +0100

    backbone app almost complete

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -7,6 +7,11 @@ var Fish = Backbone.Model.extend({
     image: 'http://www.google.com',
     description: 'Regular old fish',
     displayInfo: false
+  },
+  toggleDescription: function (){
+    let status = this.get('displayInfo');
+    console.log(this, status);
+    this.set({'displayInfo': !status});
   }
 
 });
